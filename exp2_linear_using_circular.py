63b5710e9ee30fa35dbca6c537590f17028e6258
Vinayak Prakash <vinayakprakash2121@gmail.com>
2024-11-14 20:45:15 +0530

    x

--- exp2_linear_using_circular.py
+++ exp2_linear_using_circular.py
@@ -1,19 +1,19 @@
 from numpy import *
-from matplotlib.pyplot import *
-def find_cir_conv(x1,x2):
-    N = len(x1)
-   
-    conv=zeros((N,N)) 
-    for i in range(N):
-        conv[:,i]=roll(x1,i)
-    result = dot(conv,x2)
-    return result  
-x1 = eval(input("Enter the input sequence   : "))
-x2 = eval(input("Enter the impulse sequence : "))
-l1=len(x1)
-m1=len(x2)
-N=l1+m1-1
-x1=np.pad(x1,(0,N-l1))
-x2=np.pad(x2,(0,N-m1))
-y = find_cir_conv(x1,x2)
-print("y(n)=",y)
+def circular_conv(x1,h1):
+    h1 = h1[:, newaxis]
+    x2 = zeros([len(x1),len(x1)])
+
+    for n in range(len(x1)):
+        x2[:,n] = roll(x1,n)
+    return dot(x2,h1)
+
+x1 = [1,2,3,4]
+h1 = [1,1,1]
+
+n1 = len(x1)
+n2 = len(h1)
+
+n = n1+n2-1
+x1 = pad(x1,(0,n-n1))
+h1 = pad(h1,(0,n-n2))
+print(circular_conv(x1,h1))
