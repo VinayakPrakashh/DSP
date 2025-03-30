7460f8448f5370beff4861fa795e362c5fdad856
Vinayak Prakash <vinayakprakash2121@gmail.com>
2025-03-30 16:55:06 +0530

    ss

--- dft_vs_fft.py
+++ dft_vs_fft.py
@@ -1,1 +1,36 @@
-zz
+from numpy import *
+import time
+from matplotlib.pyplot import *
+
+def DFT(N):
+    x = random.random(N)
+
+    n = arange(N)
+    k = n[:,None]
+    e = exp(-2j*pi*n*k/N)
+    X = dot(e,x)
+    return X
+def custom_fft(N):
+    x = random.random(N)
+    res = fft.fft(x)
+    return res
+def time_complexity():
+    dft_time = []
+    fft_time = []
+    gamma = arange(0, 10, 1)
+    for i in gamma:
+        N = 2**i
+        start_time = time.time()
+        DFT(N)
+        end_time = time.time()
+        dft_time.append(end_time-start_time)
+        start_time = time.time()
+        custom_fft(N)
+        end_time = time.time()
+        fft_time.append(end_time-start_time)
+    plot(gamma,dft_time,label = 'DFT_time')
+    plot(gamma,fft_time,label = 'FFT_time')
+    legend()
+    show()
+time_complexity()
+    
